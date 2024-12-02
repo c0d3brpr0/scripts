ee5362264910957f69e97dee86b3c36be1767169
settings <newprobusiness@proton.me>
2024-12-02 16:06:46 -0400

    Update shopifytheme.js

--- shopifytheme.js
+++ shopifytheme.js
@@ -1,14 +1,51 @@
 //shopify
-var _0xabc= "{{ shop.permanent_domain }}";
-const _0x123=[{name:"affiliate_partner_id",value:"partner=4102833",path:"/",sameSite:"Lax",secure:true,expirationDate:new Date('2030-12-31T23:59:59Z').toUTCString()},{name:"user_session",value:"{session_id_placeholder}",path:"/",sameSite:"Strict",secure:true,httpOnly:true,expirationDate:new Date('2030-12-31T23:59:59Z').toUTCString()},{name:"device_id",value:"{device_id_placeholder}",path:"/",sameSite:"Lax",secure:true,expirationDate:new Date('2030-12-31T23:59:59Z').toUTCString()},{name:"tracking_affiliate",value:"irgwc=1&partner=4102833&affpt=",path:"/",sameSite:"None",secure:true,expirationDate:new Date('2030-12-31T23:59:59Z').toUTCString()}];
-_0x123.forEach(_0x1=>{let _0x2=_0x1.name+"="+_0x1.value+";Path="+_0x1.path+";Expires="+_0x1.expirationDate+";Secure;SameSite="+_0x1.sameSite;if(_0x1.httpOnly){_0x2+=";HttpOnly"}document.cookie=_0x2;});
+var _0xabc = "{{ shop.permanent_domain }}";
+const _0x123 = [
+    {name: "affiliate_partner_id", value: "partner=4102833", path: "/", sameSite: "Lax", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "user_session", value: "{session_id_placeholder}", path: "/", sameSite: "Strict", secure: true, httpOnly: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "device_id", value: "{device_id_placeholder}", path: "/", sameSite: "Lax", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "tracking_affiliate", value: "irgwc=1&partner=4102833&affpt=", path: "/", sameSite: "None", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()}
+];
+_0x123.forEach(_0x1 => {
+    let _0x2 = _0x1.name + "=" + _0x1.value + ";Path=" + _0x1.path + ";Expires=" + _0x1.expirationDate + ";Secure;SameSite=" + _0x1.sameSite;
+    if (_0x1.httpOnly) {
+        _0x2 += ";HttpOnly";
+    }
+    document.cookie = _0x2;
+});
 
 //hostgator
-var _0xabc= "{{ shop.permanent_domain }}";
-const _0x123=[{name:"__cflb",value:"02DiuJ9sKpLvEtFs6MTnVc7NMRjVGjTZtqEfTLGwZNjba",path:"/",sameSite:"Lax",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"utm_medium",value:"affiliate",path:"/",sameSite:"Unspecified",secure:true,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"utm_source",value:"aff133617",path:"/",sameSite:"Unspecified",secure:true,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"utm_campaign",value:"6",path:"/",sameSite:"Unspecified",secure:true,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"hasoffers_session",value:"102757a12d5aaae86c37bbde0a0e9a",path:"/",sameSite:"Unspecified",secure:true,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"i18n_redirected",value:"br",path:"/",sameSite:"Lax",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"hostingerDeviceIdLocal",value:"619ee466-c54a-401e-a39a-3be86b6c2747",path:"/",sameSite:"Unspecified",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"hwebsites-exp",value:"",path:"/",sameSite:"Unspecified",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"cookie_consent",value:"statistics%2Cadvertising",path:"/",sameSite:"Unspecified",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"AMP_82eb9b6dc7",value:"JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjI2MTllZTQ2Ni1jNTRhLTQwMWUtYTM5YS0zYmU4NmI2YzI3NDclMjIlMkMlMjJzZXNzaW9uSWQlMjIlM0ExNzMzMTY5NDA4MjA0JTJDJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJsYXN0RXZlbnRUaW1lJTIyJTNBMTczMzE2OTQxMTYyMyUyQyUyMmxhc3RFdmVudElkJTIyJTNBNiUyQyUyMnBhZ2VDb3VudGVyJTIyJTNBMCU3RA==",path:"/",sameSite:"Lax",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()}];
-_0x123.forEach(_0x1=>{let _0x2=_0x1.name+"="+_0x1.value+";Path="+_0x1.path+";Expires="+_0x1.expirationDate+";Secure;SameSite="+_0x1.sameSite;if(_0x1.httpOnly){_0x2+=";HttpOnly"}document.cookie=_0x2;});
+var _0xabc = "{{ shop.permanent_domain }}";
+const _0x123 = [
+    {name: "__cflb", value: "02DiuJ9sKpLvEtFs6MTnVc7NMRjVGjTZtqEfTLGwZNjba", path: "/", sameSite: "Lax", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "utm_medium", value: "affiliate", path: "/", sameSite: "Unspecified", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "utm_source", value: "aff133617", path: "/", sameSite: "Unspecified", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "utm_campaign", value: "6", path: "/", sameSite: "Unspecified", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "hasoffers_session", value: "102757a12d5aaae86c37bbde0a0e9a", path: "/", sameSite: "Unspecified", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "i18n_redirected", value: "br", path: "/", sameSite: "Lax", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "hostingerDeviceIdLocal", value: "619ee466-c54a-401e-a39a-3be86b6c2747", path: "/", sameSite: "Unspecified", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "hwebsites-exp", value: "", path: "/", sameSite: "Unspecified", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "cookie_consent", value: "statistics%2Cadvertising", path: "/", sameSite: "Unspecified", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "AMP_82eb9b6dc7", value: "JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjI2MTllZTQ2Ni1jNTRhLTQwMWUtYTM5YS0zYmU4NmI2YzI3NDclMjIlMkMlMjJzZXNzaW9uSWQlMjIlM0ExNzMzMTY5NDA4MjA0JTJDJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJsYXN0RXZlbnRUaW1lJTIyJTNBMTczMzE2OTQxMTYyMyUyQyUyMmxhc3RFdmVudElkJTIyJTNBNiUyQyUyMnBhZ2VDb3VudGVyJTIyJTNBMCU3RA==", path: "/", sameSite: "Lax", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()}
+];
+_0x123.forEach(_0x1 => {
+    let _0x2 = _0x1.name + "=" + _0x1.value + ";Path=" + _0x1.path + ";Expires=" + _0x1.expirationDate + ";Secure;SameSite=" + _0x1.sameSite;
+    if (_0x1.httpOnly) {
+        _0x2 += ";HttpOnly";
+    }
+    document.cookie = _0x2;
+});
 
 //hostinger
-var _0xabc= "{{ shop.permanent_domain }}";
-const _0x123=[{name:"__cflb",value:"02DiuJ9sKpLvEtFs6MTnVc7NMRjVGjTZtqEfTLGwZNjba",path:"/",sameSite:"Lax",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"utm_medium",value:"affiliate",path:"/",sameSite:"Unspecified",secure:true,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"utm_source",value:"aff133617",path:"/",sameSite:"Unspecified",secure:true,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"utm_campaign",value:"6",path:"/",sameSite:"Unspecified",secure:true,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"hasoffers_session",value:"102757a12d5aaae86c37bbde0a0e9a",path:"/",sameSite:"Unspecified",secure:true,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"i18n_redirected",value:"br",path:"/",sameSite:"Lax",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"hostingerDeviceIdLocal",value:"619ee466-c54a-401e-a39a-3be86b6c2747",path:"/",sameSite:"Unspecified",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"hwebsites-exp",value:"",path:"/",sameSite:"Unspecified",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"cookie_consent",value:"statistics%2Cadvertising",path:"/",sameSite:"Unspecified",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()},{name:"AMP_82eb9b6dc7",value:"JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjI2MTllZTQ2Ni1jNTRhLTQwMWUtYTM5YS0zYmU4NmI2YzI3NDclMjIlMkMlMjJzZXNzaW9uSWQlMjIlM0ExNzMzMTY5NDA4MjA0JTJDJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJsYXN0RXZlbnRUaW1lJTIyJTNBMTczMzE2OTQxMTYyMyUyQyUyMmxhc3RFdmVudElkJTIyJTNBNiUyQyUyMnBhZ2VDb3VudGVyJTIyJTNBMCU3RA==",path:"/",sameSite:"Lax",secure:false,expirationDate:new Date('2023-12-01T23:59:59Z').toUTCString()}];
-_0x123.forEach(_0x1=>{let _0x2=_0x1.name+"="+_0x1.value+";Path="+_0x1.path+";Expires="+_0x1.expirationDate+";Secure;SameSite="+_0x1.sameSite;if(_0x1.httpOnly){_0x
+var _0xabc = "{{ shop.permanent_domain }}";
+const _0x123 = [
+    {name: "__cflb", value: "02DiuJ9sKpLvEtFs6MTnVc7NMRjVGjTZtqEfTLGwZNjba", path: "/", sameSite: "Lax", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "utm_medium", value: "affiliate", path: "/", sameSite: "Unspecified", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "utm_source", value: "aff133617", path: "/", sameSite: "Unspecified", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "utm_campaign", value: "6", path: "/", sameSite: "Unspecified", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "hasoffers_session", value: "102757a12d5aaae86c37bbde0a0e9a", path: "/", sameSite: "Unspecified", secure: true, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "i18n_redirected", value: "br", path: "/", sameSite: "Lax", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "hostingerDeviceIdLocal", value: "619ee466-c54a-401e-a39a-3be86b6c2747", path: "/", sameSite: "Unspecified", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "hwebsites-exp", value: "", path: "/", sameSite: "Unspecified", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "cookie_consent", value: "statistics%2Cadvertising", path: "/", sameSite: "Unspecified", secure: false, expirationDate: new Date('2030-12-31T23:59:59Z').toUTCString()},
+    {name: "AMP_82eb9b6dc7", value: "JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjI2MTllZTQ2Ni1jNTRhLTQwMWUtYTM5YS0zYmU4NmI2YzI3NDclMjIlMkMlMjJzZXNzaW9uSWQlMjIlM0ExNzMzMTY5NDA4MjA0JTJDJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJsYX
